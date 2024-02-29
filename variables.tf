0b497ac3a8f38ffa1eec51e417f883bd512d2480
khalix4 <jeremiah.abu@binghamuni.edu.ng>
2024-02-29 08:58:28 -0500

    first terraform file

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "filename" {
+    default = "/root/pet.txt"
+}
+
+variable "content" {
+    default = " we love pet"
+}
+
+variable "prefix" {
+    default = " MRS "
+}
+
+variable "seperator" {
+    default = " . "
+}
+
+variable " length " {
+    default = "1"
+}
